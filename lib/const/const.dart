451d8eecb38263df4ba8b172bcd4ded76609e0fc
yousetsu <sarujincanon@gmail.com>
2022-11-14 20:40:33 +0900

    コンスタントをファイル分割

---
+++ lib/const/const.dart
@@ -0,0 +1,23 @@
+
+
+
+
+const int helloAlarmID = 19820822;
+const String cnsGetupStatusS = '1';
+const String cnsGetupStatusF = '0';
+const bool cnsAlarmOn = true;
+const bool cnsAlarmOff = false;
+//SQL
+const String strCnsSqlCreateSetting ="CREATE TABLE IF NOT EXISTS setting(id INTEGER PRIMARY KEY, firstrun TEXT,getuptime TEXT,alarmon TEXT,kankaku INTEGER,goalgetuptime TEXT,goalsleeptime TEXT,rewardcnt INTEGER,sleepalarmtime TEXT,goalday INTEGER,mpath TEXT)";
+const String strCnsSqlInsDefSetting = "INSERT INTO setting(firstrun,getuptime,alarmon,kankaku,goalgetuptime,goalsleeptime,rewardcnt,sleepalarmtime,goalday,mpath) values('X' ,'2016-05-01 07:00:00.000Z','',1,'2016-05-01 06:00:00.000Z','2016-05-01 07:30:00.000Z',0,'',0,'')";
+const String strCnsSqlCreateRireki ="CREATE TABLE IF NOT EXISTS rireki(id INTEGER PRIMARY KEY, date TEXT, getupstatus TEXT, goalgetuptime TEXT, realgetuptime TEXT, goalbedintime TEXT, realbedintime TEXT, sleeptime TEXT)";
+const String strCnsRadDefSound = "DefaultSound";
+const String strCnsRadSelMusic = "SelectMusic";
+
+//広告ID
+//test
+const String strCnsBannerID = 'ca-app-pub-3940256099942544/6300978111'; //Banner
+const String strCnsRewardID = 'ca-app-pub-3940256099942544/5224354917'; //Reward
+//本番
+//const String strCnsBannerID = 'ca-app-pub-8759269867859745/2745032231'; //banner
+//const String strCnsRewardID = 'ca-app-pub-8759269867859745/8740337207'; //Reward
--- lib/main.dart
+++ lib/main.dart
@@ -25,21 +25,10 @@ import 'package:audio_session/audio_session.dart';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-late AudioPlayer _player;
-String strSePath = "";
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-/// Streams are created so that app can respond to notification-related events
-/// since the plugin is initialised in the `main` function
-final BehaviorSubject<ReceivedNotification> didReceiveLocalNotificationSubject =
-    BehaviorSubject<ReceivedNotification>();
-
-final BehaviorSubject<String?> selectNotificationSubject =
-    BehaviorSubject<String?>();
+import 'const/const.dart';
 
 const MethodChannel platform =
-    MethodChannel('dexterx.dev/flutter_local_notifications_example');
+MethodChannel('dexterx.dev/flutter_local_notifications_example');
 
 class ReceivedNotification {
   ReceivedNotification({
@@ -53,28 +42,21 @@ class ReceivedNotification {
   final String? body;
   final String? payload;
 }
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+/// Streams are created so that app can respond to notification-related events
+/// since the plugin is initialised in the `main` function
+final BehaviorSubject<ReceivedNotification> didReceiveLocalNotificationSubject =
+BehaviorSubject<ReceivedNotification>();
+
+final BehaviorSubject<String?> selectNotificationSubject =
+BehaviorSubject<String?>();
+
+late AudioPlayer _player;
+String strSePath = "";
+
 String? selectedNotificationPayload;
-
-const int helloAlarmID = 19820822;
-const String cnsGetupStatusS = '1';
-const String cnsGetupStatusF = '0';
-const bool cnsAlarmOn = true;
-const bool cnsAlarmOff = false;
 bool flgFirstRun = true;
-//SQL
-const String strCnsSqlCreateSetting ="CREATE TABLE IF NOT EXISTS setting(id INTEGER PRIMARY KEY, firstrun TEXT,getuptime TEXT,alarmon TEXT,kankaku INTEGER,goalgetuptime TEXT,goalsleeptime TEXT,rewardcnt INTEGER,sleepalarmtime TEXT,goalday INTEGER,mpath TEXT)";
-const String strCnsSqlInsDefSetting = "INSERT INTO setting(firstrun,getuptime,alarmon,kankaku,goalgetuptime,goalsleeptime,rewardcnt,sleepalarmtime,goalday,mpath) values('X' ,'2016-05-01 07:00:00.000Z','',1,'2016-05-01 06:00:00.000Z','2016-05-01 07:30:00.000Z',0,'',0,'')";
-const String strCnsSqlCreateRireki ="CREATE TABLE IF NOT EXISTS rireki(id INTEGER PRIMARY KEY, date TEXT, getupstatus TEXT, goalgetuptime TEXT, realgetuptime TEXT, goalbedintime TEXT, realbedintime TEXT, sleeptime TEXT)";
-const String strCnsRadDefSound = "DefaultSound";
-const String strCnsRadSelMusic = "SelectMusic";
-
-//広告ID
-//test
-const String strCnsBannerID = 'ca-app-pub-3940256099942544/6300978111'; //Banner
-const String strCnsRewardID = 'ca-app-pub-3940256099942544/5224354917'; //Reward
-//本番
-//const String strCnsBannerID = 'ca-app-pub-8759269867859745/2745032231'; //banner
-//const String strCnsRewardID = 'ca-app-pub-8759269867859745/8740337207'; //Reward
 RewardedAd? _rewardedAd;
 int _numRewardedLoadAttempts = 0;
 //-------------------------------------------------------------
